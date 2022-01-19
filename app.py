8a121488fbea97bd75c18813b45c6105c128b197
keundaf <43011822+keundaf@users.noreply.github.com>
2022-01-19 16:41:53 -0300

    Cambio de metodos y condenciacion de endpoints
    - implementacion de metodos GET y POST en endpoint /usuarios
    - implementacion de PUT, GET y DELETE para un usuario especifico usando
    endpoint /usuarios/<string:id>
    - endpoints retornan status code 200,201,400,404
    - validacion formato email

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import json
+from urllib import response
+from xmlrpc.client import ResponseError
 from flask import Flask,render_template,request,redirect, jsonify
 from models import db,Usuario,User2dic,Userlist2dic
 import uuid
 from datetime import datetime
 import psycopg2
 
+from email_validator import validate_email, EmailNotValidError
 
 app = Flask(__name__)
 
@@ -17,27 +20,45 @@ db.init_app(app)
 def create_table():
     db.create_all()
 
-@app.route('/usuario/crear', methods = ['GET','PUT'])
+@app.route('/usuarios', methods = ['GET','POST'])
 def create():
 
     if request.method == 'GET':
-        return 'JSON no recibido'
+        Usuarios = Usuario.query.all()
+        if not Usuarios:
+            return jsonify({}),204
+        else:
+            return jsonify(Userlist2dic(Usuarios)),200
 
-    if request.method == 'PUT':
+    if request.method == 'POST':
 
         usuario_nuevo = request.json
         #DATA
         ID_usuario = uuid.uuid4()
         nombre_usuario = usuario_nuevo["nombre"]
         apellido_usuario = usuario_nuevo["apellido"]
+
         email_usuario = usuario_nuevo["correo"]
+        # Validar correo.
+        try:
+            valid = validate_email(email_usuario)
+            email = valid.email
+        except EmailNotValidError as e:
+            print(str(e))
+            return jsonify({"message": "formato correo no valido"}),400
+
+        correo = Usuario.query.filter_by(Email=email).first()
+        if correo:
+            return jsonify({"message": "correo duplicado"}),400
+
+
+            
         Fecha_str = usuario_nuevo["Fecha_nacimiento"]
+
         Fecha_nacimiento = datetime.strptime(Fecha_str, '%Y-%m-%d') # utilizar regex
         #ALMACENAR
 
-        #  '''llamo el usuario existente'''
-        # usuario = Usuario.query.filter_by(ID=id).first()
-        # correo = Usuario.query.filter_by(Email=mail).first()
+        
 
         # if (usuario and not correo) or (usuario==correo):
 
@@ -49,29 +70,61 @@ def create():
         db.session.commit()
         usuario_salida = {"ID":ID_usuario,"nombre":nombre_usuario,"apellido":apellido_usuario,"correo":email_usuario,"Fecha_nacimiento":Fecha_str}
         
-        return jsonify(usuario_salida)
+        return jsonify(usuario_salida),201
 
 
 
-@app.route('/usuario')
-def RetrieveDataList():
-    Usuarios = Usuario.query.all()
-
-    Lista_Usuarios = Userlist2dic(Usuarios)
-   
-    #return render_template('lista_usuarios.html',Usuarios = Usuarios)
-    return jsonify(Lista_Usuarios)
-
-
-
-
-
-@app.route('/usuario/<string:id>', methods=['GET'])
+@app.route('/usuarios/<string:id>', methods=['GET','PUT','DELETE'])
 def RetrieveSingleEmployee(id):
+    usuario = Usuario.query.filter_by(ID=id).first()
     if request.method == 'GET':
-        usuario = Usuario.query.filter_by(ID=id).first()
-        usuario_id = User2dic(usuario)
-        return usuario_id
+        if usuario:
+            return jsonify(User2dic(usuario)),200
+
+
+
+    if request.method == 'PUT':
+        #actualizacion completa
+        usuario_requerido= request.json
+        id=usuario_requerido["ID"]
+        mail=usuario_requerido["correo"]
+        
+        '''llamo el usuario existente'''
+
+        correo = Usuario.query.filter_by(Email=mail).first()
+
+        if (usuario and not correo) or (usuario==correo):
+            db.session.delete(usuario)
+            db.session.commit()
+            #DATA
+            ID_usuario = id
+            nombre_usuario = usuario_requerido["nombre"]
+            apellido_usuario = usuario_requerido["apellido"]
+            email_usuario = usuario_requerido["correo"]
+            Fecha_str = usuario_requerido["Fecha_nacimiento"]
+            Fecha_nacimiento = datetime.strptime(Fecha_str, '%Y-%m-%d')
+            #ALMACENAR
+
+            usuario = Usuario(ID_usuario,nombre_usuario, apellido_usuario, email_usuario, Fecha_nacimiento)
+            db.session.add(usuario)
+            db.session.commit()
+
+            return redirect('/data')
+        else:
+            if not usuario:
+                return "Usuario no Existe",404
+            if correo:
+                return "Correo existente"
+
+    if request.method == 'DELETE':
+        if usuario:
+            db.session.delete(usuario)
+            db.session.commit()
+            return jsonify({"message": "usuario eliminado satisfactoriamente"}),200
+        else:
+            return jsonify({"message": "id de usuario no encontrado"}),404
+
+
 
 
 
@@ -111,18 +164,5 @@ def update():
                 return "Correo existente" 
 
 
-@app.route('/usuario/eliminar', methods=['DELETE'])
-def delete():
-    usuario_requerido= request.json
-    id=usuario_requerido["ID"]
-    usuario = Usuario.query.filter_by(ID=id).first()
-    if request.method == 'DELETE':
-        if usuario:
-            db.session.delete(usuario)
-            db.session.commit()
-            return "usuario eliminado"
-        return "usuario no encontrado"
- 
-    #return render_template('borrar.html')
 
 app.run(host='localhost', port=5000, debug=True)
